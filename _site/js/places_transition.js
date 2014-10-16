ab8fa35f723a5a7850f329d9fe16be4d095f1222
jmscarpa <joao.scarpa@gmail.com>
2014-10-16 14:57:20 -0300

    Adicionado palestrante Alexandre Tarifa

--- _site/js/places_transition.js
+++
@@ -1,9 +0,0 @@
-var places = ['estacao-da-luz', 'catedral-da-se', 'masp', 'ponte-estaiada'];
-var index = 1;
-
-var loop = setInterval(function(){
-    $(".place").removeClass("current-place");
-    $("."+places[index++]).addClass("current-place");
-
-    index = index == places.length ? 0 : index;
-}, 7000);
